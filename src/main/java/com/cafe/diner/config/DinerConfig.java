9733d4da78ed491c4f840051611f290eda628588
jenstijmen <jensvangroeningen@live.nl>
2024-03-21 11:59:36 +0100

    Zorgen dat URL's van extene services opgehaald kunnen worden

--- src/main/java/com/cafe/diner/config/DinerConfig.java
+++ src/main/java/com/cafe/diner/config/DinerConfig.java
@@ -1,4 +1,16 @@
 package com.cafe.diner.config;
 
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@Component
 public class DinerConfig {
+
+    @Value("${diner.kitchen.url}")
+    private String kitchenUrl;
+
+    @Value("${diner.bar.url}")
+    private String barUrl;
 }
